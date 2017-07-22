34ebd082dc734406233065219f08a66d59544886
Clyybber <darkmine956@gmail.com>
2017-07-22 12:21:44 +0200

    Setup Debugging and Console Output

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
+io.stdout:setvbuf("no") --For Console output in IDEs
+
+
 require "torch"
 require "cutorch"
 
 function love.load()
-  
+  if arg[#arg] == "-debug" then require("mobdebug").start() end --For Zerobrane Debugging
 end
 
 function love.update(dt)
